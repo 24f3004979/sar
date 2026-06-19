98ae3710246ab85c529ecb5d1ba2b4e5f12c158c
Madhav Rimal <madhavprapanna@gmail.com>
2026-06-19 10:05:25 +0530

    mongo crash debuging

--- infra/DB.ts
+++ infra/DB.ts
@@ -2,6 +2,14 @@
 
 import mongoose from  'mongoose';
 
+/*
+DB connection with mongodb is not working
+  Failing with unexpected crash without any traces
+
+Needs diagnosis for the given crash and get to know why even the containers are not working
+
+*/
+
 export const connectDB = async (): Promise<void> => {
   try {
     const connStr = 'mongodb://127.0.0.1:27017/sar';
